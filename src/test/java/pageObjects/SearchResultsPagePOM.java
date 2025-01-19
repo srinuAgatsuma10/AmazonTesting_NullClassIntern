6ccc607f19807a031881561da42fcecfceff1967
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-01-19 19:47:40 +0530

    general commit

--- src/test/java/pageObjects/SearchResultsPagePOM.java
+++ src/test/java/pageObjects/SearchResultsPagePOM.java
@@ -14,17 +14,30 @@ public class SearchResultsPagePOM extends BasePOM{
 	
 	// WebElements
 	/*//Brand Details*/
-	@FindBy(xpath = "")
+	@FindBy(xpath = "//ul[@data-csa-c-content-id=\"91049095031\"]//li//span[@class=\"a-list-item\"]")
 	List<WebElement> brandWithC;
 	
 	/*//Price Details*/
+	@FindBy(xpath = "//label[@aria-label='Minimum']")
+	WebElement currentPrice;
+	@FindBy(xpath = "")
+	WebElement targetPrice;
 	
 	/*//Customer rating*/
+	@FindBy(xpath = "//a[@aria-label='Apply the filter 4 Stars & Up to narrow results']")
+	WebElement custRatings;
 	
+	
+	// Validation Elements
 	/*//Result Products*/
-	@FindBy(xpath = "")
-	List<WebElement> resultProducts;
+	@FindBy(xpath = "//div[@class=\"a-section a-spacing-none puis-padding-right-small s-title-instructions-style\"]")
+	List<WebElement> resultProductsDesc;
 	
+	@FindBy(xpath = "//i[@data-cy=\"reviews-ratings-slot\"]//span[@class='a-icon-alt']")
+	WebElement resultProductsRatings;
+	
+	@FindBy(xpath = "")
+	WebElement resultProductPrice;
 	
 	
 	// Action Methods
